7fb2f121591d6304531e92bf7494e6f6b49f3f38
3vgen <jorik18901@gmail.com>
2025-09-27 18:33:25 +0700

    Create Monster class

---
+++ HaH_GameSolution/GameCore/Monster.cs
@@ -0,0 +1,14 @@
+﻿namespace GameCore
+{
+	public class Monster : Creature
+	{
+		public Monster(string name, int attack, int armor, int health, int[] damage, bool isAlive = true) : base(name)
+		{
+			Attack = attack;
+			Armor = armor;
+			Health = health;
+			Damage = damage;
+			IsAlive = isAlive;
+		}
+	}
+}
