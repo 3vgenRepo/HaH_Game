1c486539af7bb2002db8446603fc64545b0a3c15
3vgen <jorik18901@gmail.com>
2025-09-27 18:49:39 +0700

    Configure run via console view

--- HaH_GameSolution/ConsoleView/Program.cs
+++ HaH_GameSolution/ConsoleView/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GameCore;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Player player = new Player("Player1", 10, 20, 100, [1, 6]);
+		Monster monster = new Monster("Monster1", 8, 10, 100, [1, 3]);
+
+		Console.WriteLine(player.ToString());
+		Console.WriteLine(monster.ToString());
+
+		Creature.CactulateAttack(player, monster);
+	}
+}
