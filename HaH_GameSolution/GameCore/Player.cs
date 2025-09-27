735a789d4791dbb3376e64b8a2843319d8b36f4a
3vgen <jorik18901@gmail.com>
2025-09-27 17:39:29 +0700

    Create Player class

---
+++ HaH_GameSolution/GameCore/Player.cs
@@ -0,0 +1,33 @@
+﻿namespace GameCore
+{
+	public class Player : Creature
+	{
+		private int _healingCounter = 0;
+		public int HealingCounter
+		{
+			get => _healingCounter;
+			set
+			{
+				_healingCounter = _healingCounter < 4 ? value : 4;
+			}
+		}
+
+		public Player(int attack, int armor, int health, int[] damage, bool isAlive = true)
+		{
+			this.Attack = attack;
+			this.Armor = armor;
+			this.Health = health;
+			this.Damage = damage;
+			this.IsAlive = isAlive;
+		}
+
+		public void Heal()
+		{
+			if(HealingCounter <= 4)
+			{
+				Health += (int) (Health * 0.3);
+				HealingCounter++;
+			}
+		}
+	}
+}
